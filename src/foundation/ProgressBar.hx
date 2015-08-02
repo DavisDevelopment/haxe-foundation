475346e60b8cfdc0cdfbda7c0205b340079083a9
Ryan Davis <epikit@gmail.com>
2015-08-02 03:43:46 -0500

    Added "color" and "meterColor" fields to the foundation.Progressbar class

--- src/foundation/ProgressBar.hx
+++ src/foundation/ProgressBar.hx
@@ -4,6 +4,7 @@ import foundation.Pane;
 
 import tannus.html.Element;
 import tannus.math.Percent;
+import tannus.graphics.Color;
 
 class ProgressBar extends Pane {
 	/* Constructor Function */
@@ -25,6 +26,30 @@ class ProgressBar extends Pane {
 	private function set_progress(np : Percent):Percent {
 		meter.css('width', np.toString());
 		return progress;
+	}
+
+	/**
+	  * The Color of the meter
+	  */
+	public var color(get, set):Color;
+	private function get_color():Color {
+		return Color.fromString(el.css('background-color'));
+	}
+	private function set_color(nc : Color):Color {
+		el.css('background-color', nc.toString());
+		return nc;
+	}
+
+	/**
+	  * The color of the meter
+	  */
+	public var meterColor(get, set):Color;
+	private function get_meterColor() {
+		return Color.fromString(meter.css('background-color'));
+	}
+	private function set_meterColor(mc : Color):Color {
+		meter.css('background-color', mc.toString());
+		return mc;
 	}
 
 /* === Instance Fields === */
