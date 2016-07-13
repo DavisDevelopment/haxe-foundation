7d53d8c53c5025eecf3407cf16b8e0c24308696a
Ryan Davis <epikit@gmail.com>
2016-07-13 02:33:03 -0500

    finished foundation.DOMInput class

--- src/foundation/DOMInput.hx
+++ src/foundation/DOMInput.hx
@@ -1,5 +1,8 @@
 package foundation;
 
+import tannus.ds.Delta;
+import tannus.io.Signal;
+
 import foundation.TextualWidget;
 import foundation.IInput;
 
@@ -15,7 +18,10 @@ class DOMInput<T> extends Input<T> {
 	public function new():Void {
 		super();
 
+		changed = new Signal();
 		el = '<input></input>';
+
+		__listen();
 	}
 
 /* === Instance Methods === */
@@ -32,6 +38,52 @@ class DOMInput<T> extends Input<T> {
 		}
 	}
 
+	/**
+	  * Listen for changes to [this] input
+	  */
+	private function __listen():Void {
+		forwardEvent( 'change' );
+		on('change', __change);
+	}
+
+	/**
+	  * Handle a 'change' event
+	  */
+	private function __change(event : Dynamic):Void {
+		var delta = new Delta(getValue(), lastValue);
+		lastValue = getValue();
+		changed.call( delta );
+	}
+
+	/**
+	  * Add a Label to [this] Input
+	  */
+	public function label(txt : String):Void {
+		onactivate(function() {
+			if (_label == null) {
+				_label = new Label();
+				_label.owner = this;
+				after( _label );
+			}
+
+			_label.text = txt;
+		});
+	}
+
+	/**
+	  * Get the value of [this] Input
+	  */
+	override public function getValue():Null<T> {
+		return untyped iel.value;
+	}
+
+	/**
+	  * Set the value of [this] Input
+	  */
+	override public function setValue(v : Null<T>):Void {
+		iel.value = untyped v;
+	}
+
 /* === Instance Fields === */
 
 	private var ntype(get, set):String;
@@ -39,6 +91,10 @@ class DOMInput<T> extends Input<T> {
 	private inline function set_ntype(v : String):String return (iel.type = v);
 
 	/* reference to the underlying input */
-	private var iel(get, never):InputElement;
+	public var iel(get, never):InputElement;
 	private function get_iel():InputElement return cast el.at( 0 );
+
+	public var changed : Signal<Delta<T>>;
+	private var lastValue : Null<T> = null;
+	private var _label : Null<Label> = null;
 }
